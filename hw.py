96d1ccc0d1577643507bf8c40035cf5a93931043
Jorge Valdez <127890085+DordyBalbez@users.noreply.github.com>
2023-07-05 16:20:36 -0700

    Add files via upload

--- hw.py
+++ hw.py
@@ -1,224 +1,12 @@
-import numpy as np
-import sympy as sym
-import matplotlib.pyplot as plt
-import control as ct
-import control.matlab as ctm
 from ctrls import *
 
 plt.style.use('seaborn-v0_8')
 
-def pid(BW):
-
-    PM = 60                                                                          # design parameters
-    tau = 0.00
-    wc = (2/3) * BW
-
-    alpha = np.arctan(0.01*wc) + np.arctan(0.1*wc) - np.arctan(10*wc)           # solving for z
-    z = wc / np.tan(np.radians(PM) + np.pi/2 + alpha)
-
-    numK = wc * np.sqrt(((tau*wc)**2 + 1)*(wc**2 + (-0.1)**2)*((0.01*wc)**2 + 1))   # solving for controller gain k
-    denK = np.sqrt((wc**2 + z**2)*((-0.01*wc)**2 + 1))
-    k = numK/denK
-
-    numL = [0, -0.01*k, k*(1-0.02*z), 2*k*z-0.01*z**2, z**2]                        # specific to this problems plant
-    denL = [0.01*tau, 0.999*tau + 0.01, 0.999 - 0.1*tau, -0.1, 0]
-
-    L = ct.TransferFunction(numL, denL)                                            # algebra to get transfer functions
-    T = L / (1 + L)
-    S = 1 - T
-
-    gm, pm, wcg, wcp = ct.margin(T)                                                # get margins
-
-    plt.figure()
-    ct.nyquist(T)
-    plt.title(r"Nyquist plot of $T(s)$; $BW=$"+str(BW))
-
-    plt.figure()
-    ct.bode(T)
-    plt.suptitle(r"Bode Plot of $T(s)$; $BW=$"+str(BW))
-
-    plt.figure()
-    ct.bode(S)
-    plt.suptitle(r"Bode Plot of $S(s)$; $BW=$"+str(BW))
-
-    plt.figure()
-    ct.step_response(T)
-    plt.title(r"Step response of $T(s)$; $BW=$"+str(BW))
-
-    plt.figure()
-    ct.step_response(S)
-    plt.title(r"Step response of $S(s)$; $BW=$"+str(BW))
-
-
-    print(
-    "PM + 90 deg + alpha = ", (PM + 90 + alpha), '\n'
-    '\n'
-    "Design specs: \n"
-        "Bandwidth:", BW,"\n"       
-        "Phase margin: 60 deg\n"
-        'Tau: 0.01\n'
-        'Omega c = 0.667 * BW\n'
-    '\n'
-    "results: \n"
-        "k = ", k,'\n'
-        'z = ', z,'\n'
-    '\n'
-    'Closed-loop params: \n'
-        'Gain margin: ', gm, '\n'
-        'Gain Crossover freq:', wcg,'\n'
-        'Phase margin:', pm,'\n'
-        'Phase crossover freq:', wcp
-    )
 
 def prob1():
-    A = np.array([[0.2, 0.2],[0.2, 0.2]])
-    B = np.array([[0.], [1.]]).reshape(2, 1)
-    C = np.array([0., 1.]).reshape(1, 2)
-    D = np.array([0.])
-
-    x0 = np.array([1, 0]).reshape(2, 1)
-
-    x = []
-    y = []
-
-    for k in range(1, 6):
-        S = np.array([0.0, 0.0]).reshape(2, 1)
-        for j in range(1, k):
-            S = S + np.linalg.matrix_power(A, k - j) @ B
-        xk = np.linalg.matrix_power(A, k) @ x0 + B + S
-        x.append(xk)
-
-    for k in range(1, 6):
-        S = np.array([0.0, 0.0]).reshape(2, 1)
-        for j in range(1, k):
-            S = S + C @ np.linalg.matrix_power(A, k - j) @ B
-        yk = C @ (np.linalg.matrix_power(A, k) @ x0 + B + S) + D
-        y.append(yk)
-
-    print(x, '\n', y)
-
-def c2d_feuler(H, T):
-    H = ct.tf(H)
-
-    z = sym.Symbol('z')
-    s = (z - 1) / T
-
-    num, den = ct.tfdata(H)
-
-    num = num[0][0]
-    nums = sym.Poly.from_list(num, s)
-    den = den[0][0]
-    dens = sym.Poly.from_list(den, s)
-
-    Hz = sym.simplify(nums / dens)
-    sym_num = sym.numer(Hz)
-    sym_den = sym.denom(Hz)
-
-    try:
-        num = sym.Poly(sym_num).all_coeffs()
-        num = list(map(float, num))
-    except Exception:
-        num = [1]
-        num = list(map(float, num))
-    finally:
-        den = sym.Poly(sym_den).all_coeffs()
-        den = list(map(float, den))
-
-    Hz = ct.tf(num, den, dt=T)
-    Hz = ct.ss(Hz)
-    return(Hz)
-
-def prob5(H, T):
-    Htust = ctm.c2d(H, T, 'tustin')
-    Hzoh = ctm.c2d(H, T, 'zoh')
-    Hfe = ct.tf(c2d_feuler(H, T))
-
-    plt.figure()
-    ct.bode([H, Hfe, Htust, Hzoh])
-    plt.figlegend(['CT', 'Forward Euler', 'Tustin', 'ZOH'])
-    plt.legend(['CT', 'For Eu', 'Tust', 'ZOH'])
-    plt.suptitle('T=' + str(T))
-
-def lab3(BW, tau, PM):
-    P = ct.tf([-2.776e-17, 0.165555],[1, -0.1995, 0])
-    wc = 2 * BW / 3
-    beta = np.radians(PM) + np.arctan(tau*wc) + np.arctan(wc / 0.1995) - np.arctan(2.77e-17 / 0.1655)
-
-    a = wc / (np.tan(beta / 2))
-
-    num = (wc**2) * np.sqrt(((tau*wc)**2 + 1) * (wc**2 + 0.1995**2))
-    den = (wc**2 + a**2) * np.sqrt(-2.77e-17**2 + 0.1655**2)
-
-    K = 2 * num / den
-    print('K=', K, '\n a=', a, '\n')
-
-    C = K * ct.tf([1, a],[1, 0]) * ct.tf([1, a],[tau, 1])
-    print(C)
-
-    Ki = K*(a**2)
-    Kp = K * (2*a - tau*a**2)
-    Kd = K - 2*a*K*tau - K*(a*tau)**2
-
-    return(Ki, Kp, Kd)
-
-def hwprob2():
-    P1s = ct.tf([-1, 5], [1, 1]) * ct.tf([1], [1, 1]) * ct.tf([1], [1, 1])
-    P2s = ct.tf([5], [1, 1, 1])
-
-    ### PM TUNING METHODS ###
-
-    BW = 10
-    wc = 2 * BW / 3
-    tau = wc/10
-
-    theta = -np.pi + np.radians(50)
-    beta = -np.pi/2 - np.arctan(tau * wc) - np.arctan(wc/5) - 3 * np.arctan(wc)
-    a1 = wc / np.tan((theta - beta) / 2)
-
-    alpha = -np.pi/2 - np.arctan(tau * wc) - np.arctan(2 * wc / 0.5+0.866j) - np.arctan(2 * wc / 0.5 - 0.866j)
-    a2 = wc / np.tan((theta - alpha) / 2)
-
-def prob3(BW, PM):
-    fs = 10
-    Ts = 1/fs
-    wc = 2 * BW / 3
-    tau = wc/10
-    P = ct.tf([-1, 1],[1, 1]) * ct.tf([1],[1, 1])
-
-    theta = -np.pi/2 + np.radians(PM)
-    beta = -3 * np.arctan(wc) - np.arctan(tau * wc)
-    ZOHlag = -(Ts*wc/2)
-    a = wc / np.tan((theta - beta - ZOHlag) / 2)
-
-    num = wc * (wc**2 + 1) * np.sqrt((tau * wc)**2 + 1)
-    den = (wc**2 + a**2) * np.sqrt((-wc)**2 + 1)
-    K = num / den
-
-    C = K * ct.tf([1, a],[1, 0]) * ct.tf([1, a],[tau, 1])
-
-    Pdt = ct.c2d(P, Ts, 'zoh')
-    Cdt = ct.c2d(C, Ts, 'tustin')
-    Ldt = Pdt * Cdt
-    Tdt = Ldt / (1 + Ldt)
-    Sdt = 1 - Tdt
-
-    plt.figure()
-    ct.bode([Tdt, Sdt])
-    plt.suptitle('Closed Loop Bode')
-
-    plt.figure()
-    t, y = ct.step_response(Tdt)
-    plt.plot(t, y)
-    plt.title("Closed Loop Step")
-    plt.ylabel("Amplitude")
-    plt.xlabel("Time (s)")
-
-    plt.figure()
-    ct.nyquist(Tdt)
-    plt.title('Closed Loop Nyquist')
-def prob1():
-    cv = cw = 0.1
+    c_v = c_w = 0.1
     m = 0.2
+    l = 0.2
     M = 0.4
     g = 9.81
     R = 5
@@ -226,18 +14,18 @@ def prob1():
     k = 0.3
     r = 0.1
 
-    pout = 75 * ct.tf([1, 15.6],[1, 49]) * ct.tf([1, -3.1],[1, 22]) * ct.tf([1],[1, -3.3]) * ct.tf([1],[1, 0.47])
-    pin = ct.tf([375, 0],[1, 49]) * ct.tf([1],[1, 22]) * ct.tf([1],[1, -3.3]) * ct.tf([1],[1, 0.47])
-    p3 = ct.tf([1],[1])
+    pout = 75 * ct.tf([1, 15.6], [1, 49]) * ct.tf([1, -3.1], [1, 22]) * ct.tf([1], [1, -3.3]) * ct.tf([1], [1, 0.47])
+    pin = ct.tf([375, 0], [1, 49]) * ct.tf([1], [1, 22]) * ct.tf([1], [1, -3.3]) * ct.tf([1], [1, 0.47])
+    p3 = ct.tf([1], [1])
     fs = 100
-    Ts = 1/fs
+    Ts = 1 / fs
     pm = 50
 
     pout_ss = ct.tf2ss(p1)
     pin_ss = ct.tf2ss(p2)
 
-    pin_w = ct.tf([0.003136, -0.003312, -0.002149, 0.00229],[1,  -3.444, 4.391, -2.543, 0.5058])
-    pout_w = ct.tf([-0.0001696, -0.3351, 69.24, 931.1, -3542],[1, 67.12, 853.2, -3087, -1633])
+    pin_w = ct.tf([0.003136, -0.003312, -0.002149, 0.00229], [1, -3.444, 4.391, -2.543, 0.5058])
+    pout_w = ct.tf([-0.0001696, -0.3351, 69.24, 931.1, -3542], [1, 67.12, 853.2, -3087, -1633])
 
     zeros_in = np.roots(pin_w.num[0][0])
     poles_in = np.roots(pin_w.den[0][0])
@@ -247,11 +35,80 @@ def prob1():
     maxpole = np.max(np.abs(poles_in), np.abs(poles_out))
     wc = np.ceil(maxpole)
 
-    theta = -np.pi/2 + np.radians(pm)
+    theta = -np.pi / 2 + np.radians(pm)
     a_in = b_in = 0
     a_out = b_out = 0
     for z in zeros_in:
-        a_in = a_in + np.arctan(wc/z)
+        a_in = a_in + np.arctan(wc / z)
 
 
+def prob2():
+    f = 10
+    Ts = 1 / f
+    bandwidth = 6
+    phase_margin = 45
+    P = ct.tf([0.01914, -0.5815, 3.973], [1, 3.987, 3.973])  # from MATLAB (python has no d2c() command yet)
+
+    Cct = pid_ct(P, bandwidth, phase_margin)  # script for designing PID based on in-class techniques
+    Lct = P * Cct
+    Tct = Lct / (1 + Lct)
+    Sct = 1 - Tct
+
+    Pdt = ct.c2d(P, Ts, 'zoh')
+    Cdt = ct.c2d(Cct, Ts, 'tustin')
+    print(f'C(z)={Cdt}')
+
+    Tdt = Pdt * Cdt / (1 + Pdt * Cdt)
+    Sdt = 1 - Tdt
+
+    plt.figure()
+    ct.bode(Pdt * Cdt, margins=True)
+    plt.suptitle(r'$L(e^{j\omega})$ frequency response')
+
+    plt.figure()
+    ct.bode([Tdt, Sdt])
+    plt.suptitle(r'$T(e^{j\omega})$ & $S(e^{j\omega})$ frequency responses')
+
+    plt.figure()
+    step(Tdt)
+    plt.title('$T(s)$ step response')
+
+    # part 2
+
+    D = ct.tf([1], [1, 1])
+    mp = ct.dcgain(P)
+    md = ct.dcgain(D)
+    H = 1 / (mp * md)
+    print(f'H={H}')
+
+    Tff = Sct * P * Cct + Sct * D - Sct * P * H
+
+    plt.figure()
+    ct.bode(Tff)
+
+    plt.figure()
+    step(Tff)
+    plt.title('Feed Forward Closed Loop step response')
+
+    Tff = ct.c2d(Tff, Ts, 'tustin')
+    print(f'T={Tff}')
+
+    # plt.figure()
+    # ct.bode([Tff, Tdt])
+    #
+    # plt.figure()
+    # step(Tff)
+
+def obs_ex():
+    p = ct.tf([1, 2],[1, 3]) * ct.tf([1], [1, 4]) # want closed-loop poles of observer at -2+2i, -2-2i
+    pss = ct.tf2ss(p)
+    A = pss.A[0][0]
+    B = pss.B[0][0]
+    C = pss.C[0][0]
+
+    K = np.array([0, 1]).reshape(1, 2) @ la.inv(np.hstack(B, A @ B)) @ (la.matrix_power(A, 2) + 4 * A + 8)
+    L = np.array([0, 1]).reshape(1, 2) @ la.inv(np.hstack(C.T, A.T @ C.T)) @ (la.matrix_power(A.T, 2) + 4 * A.T + 8)
+
+    print(L.T)
+    return(L.T)
 
